650a86bfb368b56d52aa3703d64cc39297eb4d99
MrJookie <MrJookie@users.noreply.github.com>
2015-07-29 02:10:20 +0200

    Test

--- src/Application.cpp
+++ src/Application.cpp
@@ -70,7 +70,7 @@ void Application::createWindow(sf::Uint16 width, sf::Uint16 height, bool fullscr
 
     window.setFramerateLimit(10000);
 	window.setKeyRepeatEnabled(true);
-	window.setVerticalSyncEnabled(false);
+	//window.setVerticalSyncEnabled(false);
 }
 
 void Application::loadResources()
